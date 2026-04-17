eeb44ca48c670960a2de84f04a078cb175578ef3
SergiuIliuta <sergiu.iliuta@theaccessgroup.com>
2026-04-17 10:53:45 +0300

    fix(ts): update JSX types for Web Components to be compatible with React 18+

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,12 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    'mlm-header': any;
-    'mlm-footer': any;
+/// <reference types="vite/client" />
+
+import * as React from 'react'
+
+declare module 'react' {
+  namespace JSX {
+    interface IntrinsicElements {
+      'mlm-header': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      'mlm-footer': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    }
   }
 }
